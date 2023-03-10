c71611f79ecdb7d383fd84b2f6e5a4f4826a6b51
Josh Gulledge <jgulledge19@gmail.com>
2023-03-09 17:47:46 -0700

    Changed the contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,7 +64,7 @@ export default function Contact() {
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            Hire Me
+            Contact Me
           </h2>
           <p className="leading-relaxed mb-5">
             I would appriciate any opportunity or feed back you may have. Feel free to reach out and have a great day! 

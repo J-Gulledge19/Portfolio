0cb6427b9261197fe9d842b083b46c182ed78cdf
Josh Gulledge <jgulledge19@gmail.com>
2023-05-23 17:23:08 -0600

    Input current work to main page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
+import CurrentWork from './components/Currentwork';
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
@@ -11,6 +12,7 @@ function App() {
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
+      <CurrentWork />
       <Projects />
       <Skills />
       <Testimonials />

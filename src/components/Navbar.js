2bd997848624b6a512c7fb7dbf292f91838d88db
Josh Gulledge <jgulledge19@gmail.com>
2023-05-23 17:38:47 -0600

    Added in current work nav button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,6 +10,9 @@ export default function Navbar() {
           </a>
         </h1>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#currentwork" className="mr-5 hover:text-white">
+            Active Projects
+          </a>
           <a href="#projects" className="mr-5 hover:text-white">
             My Projects
           </a>

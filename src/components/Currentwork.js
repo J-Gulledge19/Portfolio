54ba0a521ee8e219be13a327759cf3f8106c7995
Josh Gulledge <jgulledge19@gmail.com>
2023-08-07 16:34:22 -0600

    Changed Icon spacing and header decoration

--- src/components/Currentwork.js
+++ src/components/Currentwork.js
@@ -11,13 +11,13 @@ export default function CurrentWork() {
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-stone-300">
             Current Projects
           </h1>
-            <h1 className="grid grid-cols-2 gap-4 place-content-start h-8 sm:text-2xl text-2xl font-medium title-font mt-4 text-stone-300">Sayner Mobil</h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-stone-300">
-            <GiSilverBullet className="w-8 h-20 inline-block mb-4"/>Building website for a gas station in northern Wisconsin. Currenlty in the planning stages awaiting customers specifications.</p>
+            <h1 className="grid grid-cols-2 gap-4 place-content-start h-8 sm:text-2xl text-2xl font-medium title-font mt-4 text-stone-300 text-decoration-line: underline">Sayner Mobil</h1>
+          <p className="lg:w-3/4 mx-auto leading-relaxed text-base text-stone-300">
+            <GiSilverBullet className="w-8 h-20 inline-block mb-4 mr-5"/>Building website for a gas station in northern Wisconsin. Currenlty in the planning stages awaiting customers specifications.</p>
             <br></br>
-            <h1 className="grid grid-cols-2 gap-4 place-content-start h-8 sm:text-2xl text-2xl font-medium title-font mt-4 text-stone-300">Job Tracker</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-stone-300"> 
-            <GiSilverBullet className="w-8 h-20 inline-block mb-4"/>Rebuilding my Sheetmetal Job Tracker website using Python, React and adding in Mobx for scalability. Hoping to make it easier for sheetmetal shops to track a job's downloads and due dates.
+            <h1 className="grid grid-cols-2 gap-4 place-content-start h-8 sm:text-2xl text-2xl font-medium title-font mt-4 text-stone-300 text-decoration-line: underline">Job Tracker</h1>
+            <p className="lg:w-3/4 mx-auto leading-relaxed text-base text-stone-300"> 
+            <GiSilverBullet className="w-8 h-20 inline-block mb-4 mr-5"/>Rebuilding my Sheetmetal Job Tracker website using Python, React and adding in Mobx for scalability. Hoping to make it easier for sheetmetal shops to track a job's downloads and due dates.
           </p>
         </div>
       </div>

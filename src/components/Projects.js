da7c0a44664253f40561edc06af2993b784df3c7
Josh Gulledge <jgulledge19@gmail.com>
2023-08-09 17:33:07 -0600

    Changed padding

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -35,7 +35,7 @@ export default function Projects() {
                 </div>
               </div>
                 <a href={project.link}>
-                  <button class="relative inline-flex items-center justify-center p-0.5 mt-2 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                  <button class="relative inline-flex items-center justify-center p-0.5  mt-2 mb-4 mr-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
                     <span class="relative px-4 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                       Live Link
                     </span>

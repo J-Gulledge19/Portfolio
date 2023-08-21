0f1017c841788dcd1f521809b66f24827418637d
Josh Gulledge <jgulledge19@gmail.com>
2023-08-21 16:43:03 -0600

    Changed contact style and removed unused code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
-        <h1 className="title-font font-medium text-stone-300 hover:text-blue-500 mb-4 md:mb-0">
+        <h1 className="title-font font-medium text-stone-300 hover:text-blue-500 hover:scale-110 mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Josh Gulledge
           </a>
@@ -31,19 +31,13 @@ export default function Navbar() {
             <span class="relative">Testimonials</span>
           </a>
         </nav>
-        <a
-          href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-stone-300 mt-4 md:mt-0 no-underline hover:underline">
-          Contact Me
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
+        <a href="#contact" class="relative inline-flex items-center justify-center px-2 py-0 mr-1 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group">
+          <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-blue-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+          <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
+          <span class="relative inline-flex items-center">Contact Me <ArrowRightIcon className="w-4 h-4 ml-1" /></span>
         </a>
       </div>
     </header>
   );
 }
 
-<a href="#testimonials" class="relative inline-flex items-center justify-center px-2 py-1 mr-1 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group">
-<span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-blue-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
-<span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-<span class="relative">Testimonials</span>
-</a>

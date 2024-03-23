ea7ebf76f7552198e26d214b2bb9aa326360f1ed
Josh Gulledge <jgulledge19@gmail.com>
2024-03-22 21:50:48 -0600

    Fixed styling for submit pop up

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -118,14 +118,14 @@ export default function Contact() {
                 {
                     close => (
                         <div className='p-8 mt-10'>
-                            <div className='bg-white w-1/2 mx-auto p-4 rounded-md shadow-lg bg-gray-50'>
-                                <h1 className="text-2xl font-bold text-blue-500 mb-4">
+                            <div className='bg-gray-50 w-1/2 mx-auto p-4 rounded-md shadow-2xl'>
+                                <h1 className="text-xl font-bold text-stone-800 mb-4">
                                   Message sent! Thank you for reaching out! I will get back to you as soon as possible.
                                 </h1>
                               <div className="text-right">
-                                <button className="inline-block bg-blue-500 py-2 px-4 text-white rounded-md font-semibold uppercase text-sm " onClick=
+                                <button className="font-large shadow-xl w-20 h-10 text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white" onClick=
                                     {() => close()}>
-                                        Close modal
+                                        Close
                                 </button>
                               </div>
                             </div>

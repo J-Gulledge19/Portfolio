f45f626f694024d9d7348e02bf7c7af697130b44
Josh Gulledge <jgulledge19@gmail.com>
2024-07-08 13:19:20 -0600

    Rounded images and hover description along with base shadow

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,10 +20,10 @@ export default function Projects() {
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 w-full h-full object-cover object-center rounded-2xl shadow-2xl shadow-black"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100 rounded-xl">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
